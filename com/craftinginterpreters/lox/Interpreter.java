83687ec40f4d7bbd932014d9a4620a3d424ee7cc
dodo <5515159@gmail.com>
2022-06-30 17:20:29 +0800

    add support for class

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -67,6 +67,22 @@ class Interpreter implements Expr.Visitor<Object>,
         executeBlock(stmt.statements, new Environment(environment));
         return null;
     }
+    
+    @Override 
+    public Void visitClassStmt(Stmt.Class stmt) {
+        environment.define(stmt.name.lexeme, null);
+
+        Map<String, LoxFunction> methods = new HashMap<>();
+        for (Stmt.Function method : stmt.methods) {
+            LoxFunction function = new LoxFunction(method, environment,
+                 method.name.lexeme.equals("init"));
+            methods.put(method.name.lexeme, function);
+        }
+
+        LoxClass klcass = new LoxClass(stmt.name.lexeme, methods);
+        environment.assign(stmt.name, klcass);
+        return null;
+    }
 
     @Override 
     public Void visitExpressionStmt(Stmt.Expression stmt) {
@@ -76,7 +92,7 @@ class Interpreter implements Expr.Visitor<Object>,
 
     @Override 
     public Void visitFunctionStmt(Stmt.Function stmt) {
-        LoxFunction function = new LoxFunction(stmt, environment);
+        LoxFunction function = new LoxFunction(stmt, environment, false);
         environment.define(stmt.name.lexeme, function);
         return null;
     }
@@ -155,6 +171,24 @@ class Interpreter implements Expr.Visitor<Object>,
         }
 
         return evaluate(expr.right);
+    }
+
+    @Override 
+    public Object visitSetExpr(Expr.Set expr) {
+        Object object = evaluate(expr.object);
+
+        if (!(object instanceof LoxInstance)) {
+            throw new RuntimeError(expr.name, "Only instances have fields.");
+        }
+
+        Object value = evaluate(expr.value);
+        ((LoxInstance)object).set(expr.name, value);
+        return value;
+    }
+
+    @Override 
+    public Object visitThisExpr(Expr.This expr) {
+        return lookUpVariable(expr.keyword, expr);
     }
 
     @Override
@@ -297,5 +331,16 @@ class Interpreter implements Expr.Visitor<Object>,
         }
         return function.call(this, arguments);
         
+    }
+
+    @Override 
+    public Object visitGetExpr(Expr.Get expr) {
+        Object object = evaluate(expr.object);
+        if (object instanceof LoxInstance) {
+            return ((LoxInstance)object).get(expr.name);
+        }
+
+        throw new RuntimeError(expr.name, 
+        "Only instance have properties.");
     }
 }
